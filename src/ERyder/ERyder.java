4784676d7a3684d8c77e236142fcaffac8f98f2a
康柏图 <751387925@qq.com>
2026-03-11 00:56:01 +0800

    exercise4

--- src/ERyder/ERyder.java
+++ src/ERyder/ERyder.java
@@ -1,24 +1,45 @@
 package ERyder;
 
 public class ERyder {
-   private int bikeID;
+   private String bikeID;
    private int batteryLevel;
    private boolean isAvailable;
-   private int kmDriven;
+   private double kmDriven;
 
-   public ERyder(int var1, int var2, boolean var3, int var4) {
-      this.bikeID = var1;
-      this.batteryLevel = var2;
-      this.isAvailable = var3;
-      this.kmDriven = var4;
-      if (var2 <= 0) {
+
+
+   private static final String COMPANY_NAME = "ERyder";
+   private static final double BASE_FARE = 1.0;
+   private static final double PER_MINUTE_FARE = 0.5;
+   private final String LINK_PHONE_NUMBER;
+   private final String LINKED_ACCOUNT;
+   private double totalUsageInMinutes;
+   private double totalFare;
+
+   public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven){
+      
+      this.bikeID = bikeID;
+      this.batteryLevel = batteryLevel;
+      this.isAvailable = isAvailable;
+      this.kmDriven = kmDriven;
+      this.LINK_PHONE_NUMBER = "N/A";
+      this.LINKED_ACCOUNT = "N/A";
+   };
+   public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven, String LINK_PHONE_NUMBER, String LINKED_ACCOUNT){
+      this.bikeID = bikeID; 
+      this.batteryLevel = batteryLevel;
+       this.isAvailable = isAvailable;
+       this.kmDriven = kmDriven;
+       this.LINK_PHONE_NUMBER = LINK_PHONE_NUMBER;
+       this.LINKED_ACCOUNT = LINKED_ACCOUNT;
+      this.isAvailable = isAvailable;
+      this.kmDriven = kmDriven;
+      if (batteryLevel <= 0) {
          this.isAvailable = false;
       } else {
          this.isAvailable = true;
       }
-
-   }
-
+   } 
    public void ride() {
       if (this.isAvailable) {
          this.batteryLevel -= 10;
@@ -34,12 +55,38 @@ public class ERyder {
    }
 
    public void printBikeDetails() {
-      int var10001 = this.bikeID;
+      String var10001 = this.bikeID;
       System.out.println("Bike ID: " + var10001);
-      var10001 = this.batteryLevel;
+      var10001 = String.valueOf(this.batteryLevel);
       System.out.println("Battery Level: " + var10001);
       System.out.println("Availability: " + (this.isAvailable ? "Available" : "Not Available"));
-      var10001 = this.kmDriven;
+      var10001 = String.valueOf(this.kmDriven);
       System.out.println("Kilometers Driven: " + var10001);
+   }
+   public void printRideDetails(int usageInMinutes) {
+      String var10001 = this.LINKED_ACCOUNT;
+      System.out.println("Linked Phone Number: " + var10001);
+      var10001 = this.LINK_PHONE_NUMBER;
+      System.out.println("Linked Account: " + var10001);
+      var10001 = this.bikeID;
+      System.out.println("Bike ID: " + var10001);
+      var10001 = String.valueOf(usageInMinutes);
+      System.out.println("Usage Time (minutes): " + var10001);
+      totalFare = calculateFare(usageInMinutes);
+      var10001 = String.valueOf(totalFare);
+      System.out.println("Total Fare: $" + var10001);
+
+   }
+   private double calculateFare(int usageInMinutes) {
+      return BASE_FARE + (PER_MINUTE_FARE * usageInMinutes);
+   }
+   public int getBatteryLevel() {
+      return this.batteryLevel;
+   }
+   public void setBatteryLevel(int batteryLevel) {
+      this.batteryLevel = batteryLevel;
+   }
+   public double getCalulatedFare(int usageInMinutes) {
+      return calculateFare(usageInMinutes);
    }
 }
--- src/ERyder/main.java
+++ src/ERyder/main.java
@@ -1,9 +1,14 @@
 package ERyder;
 
 public class main{
-    public static void main(String[] args) {
-        ERyder bike1 = new ERyder(101, 80, true, 0);
+    public static void main (String[] args) {
+        ERyder bike1 = new ERyder("BIKE001", 80, true, 5.0);
         bike1.printBikeDetails();
-        bike1.ride();
+        bike1.printRideDetails(0);
+        ERyder bike2 = new ERyder("BIKE002", 60, false, 10.5, "0987654321", "ACCOUNT002");
+        bike2.ride();
+        bike2.printBikeDetails();
+        bike2.printRideDetails(30);
+        System.out.println("Calculated Fare: $" + bike2.getCalulatedFare(30));
     }
 }
