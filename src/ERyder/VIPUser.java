663de79364bed09960419e669af121cfc7360da5
751387925-tech <751387925@qq.com>
2026-04-06 23:39:47 +0800

    11

--- src/ERyder/VIPUser.java
+++ src/ERyder/VIPUser.java
@@ -3,12 +3,6 @@ public class VIPUser extends RegisteredUsers {
     
     public VIPUser(String fullName, String emailAddress, String dateOfBirth, long cardNumber, String cardExpiryDate, String cardProvider, int cvv) {
         super(fullName, emailAddress, dateOfBirth, cardNumber, cardExpiryDate, cardProvider, cvv);
-    if (userType.equalsIgnoreCase("VIP")) {
-        newUser = new VIPUser(fullName, emailAddress, dateOfBirth, cardNumber, cardExpiryDate, cardProvider, cvv);}
-    else {
-        newUser = new RegularUser(fullName, emailAddress, dateOfBirth, cardNumber, cardExpiryDate, cardProvider, cvv, "Regular");
-    }
-        RegisteredUsersList.add(newUser);
     }
     @Override
     public double calculateFare(double baseFare) {
